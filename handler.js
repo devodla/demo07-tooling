8b60c2a5af95ae00bc096735eb78b74df95670ff
Huillca, Reynaldo <reynaldo.huillca@btgpactual.com>
2021-06-22 12:42:44 -0300

    test: testing endpoint in local

--- handler.js
+++ handler.js
@@ -5,8 +5,7 @@ module.exports.hello = async (event) => {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v2.0! Your function executed successfully!',
-        input: event,
+        message: 'Hello!'
       },
       null,
       2
